076a486a0819f7ee57080b53a558c67dc88c721c
Douglas Jovenil <douglascastilhos@yahoo.com.br>
2020-11-05 03:59:17 -0300

    done: MVP

---
+++ README.md
@@ -0,0 +1,23 @@
+# LoRa Device
+
+## Dependencies 
+```
+Platformio extension
+Microsoft C/C++ extension
+```
+
+## Commands
+```
+pio run -> Compila
+pio run -t upload -> Compila e escrever o firmware
+pio device list -> mostra as placas
+pio run -t upload --upload-port COM3
+pio device monitor -b 115200
+```
+
+Roteadores e frequencias -> https://www.thethingsnetwork.org/docs/gateways/packet-forwarder/semtech-udp.html
+
+Frequencias -> https://www.thethingsnetwork.org/docs/lorawan/frequency-plans.html
+
+Frequencias por pais -> https://www.thethingsnetwork.org/docs/lorawan/frequencies-by-country.html
+
---
+++ src/main.cpp
@@ -0,0 +1,192 @@
+#include <Arduino.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
+// 0x70.
+static const u1_t PROGMEM APPEUI[8] = {0x00, 0x00, 0x00, 0x00,
+                                       0x00, 0x00, 0x00, 0x00};
+void os_getArtEui(u1_t* buf) { memcpy_P(buf, APPEUI, 8); }
+
+// 3a 7f 8d f4 5a 08 ac 6b LSB
+// 0x3a, 0x7f, 0x8d, 0xf4, 0x5a, 0x08, 0xac, 0x6b
+
+// This should also be in little endian format, see above.
+static const u1_t PROGMEM DEVEUI[8] = {0x3a, 0x7f, 0x8d, 0xf4,
+                                       0x5a, 0x08, 0xac, 0x6b};
+void os_getDevEui(u1_t* buf) { memcpy_P(buf, DEVEUI, 8); }
+
+// 91 28 bf bd d7 fa 42 c7 7a ff 8d c8 40 7e 93 89 MSB
+// 0x91, 0x28, 0xbf, 0xbd, 0xd7, 0xfa, 0x42, 0xc7, 0x7a, 0xff, 0x8d, 0xc8, 0x40,
+// 0x7e, 0x93, 0x89
+
+// This key should be in big endian format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from ttnctl can be copied as-is.
+static const u1_t PROGMEM APPKEY[16] = {0x91, 0x28, 0xbf, 0xbd, 0xd7, 0xfa,
+                                        0x42, 0xc7, 0x7a, 0xff, 0x8d, 0xc8,
+                                        0x40, 0x7e, 0x93, 0x89};
+void os_getDevKey(u1_t* buf) { memcpy_P(buf, APPKEY, 16); }
+
+static uint8_t mydata[] = "Hello, world!";
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 10;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 18,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 14,
+    .dio = {26, 33, LMIC_UNUSED_PIN},
+};
+
+void printHex2(unsigned v) {
+  v &= 0xff;
+  if (v < 16) Serial.print('0');
+  Serial.print(v, HEX);
+}
+
+void do_send(osjob_t* j) {
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, mydata, sizeof(mydata) - 1, 0);
+    Serial.println(F("Packet queued"));
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void onEvent(ev_t ev) {
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch (ev) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      {
+        u4_t netid = 0;
+        devaddr_t devaddr = 0;
+        u1_t nwkKey[16];
+        u1_t artKey[16];
+        LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+        Serial.print("netid: ");
+        Serial.println(netid, DEC);
+        Serial.print("devaddr: ");
+        Serial.println(devaddr, HEX);
+        Serial.print("AppSKey: ");
+        for (size_t i = 0; i < sizeof(artKey); ++i) {
+          if (i != 0) Serial.print("-");
+          printHex2(artKey[i]);
+        }
+        Serial.println("");
+        Serial.print("NwkSKey: ");
+        for (size_t i = 0; i < sizeof(nwkKey); ++i) {
+          if (i != 0) Serial.print("-");
+          printHex2(nwkKey[i]);
+        }
+        Serial.println();
+      }
+      // Disable link check validation (automatically enabled
+      // during join, but because slow data rates change max TX
+      // size, we don't use it in this example.
+      LMIC_setLinkCheckMode(0);
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      break;
+    case EV_TXCOMPLETE:
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+      if (LMIC.txrxFlags & TXRX_ACK) Serial.println(F("Received ack"));
+      if (LMIC.dataLen) {
+        Serial.print(F("Received "));
+        Serial.print(LMIC.dataLen);
+        Serial.println(F(" bytes of payload"));
+        Serial.print("The payload is: ");
+        
+         for (int i = 0; i < LMIC.dataLen; i++) {
+          // Serial.print(LMIC.frame[LMIC.dataBeg + i], HEX);
+          Serial.print((char)LMIC.frame[LMIC.dataBeg + i]);
+        }
+        Serial.println();
+      }
+      // Schedule next transmission
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL),
+                          do_send);
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
+    case EV_RESET:
+      Serial.println(F("EV_RESET"));
+      break;
+    case EV_RXCOMPLETE:
+      // data received in ping slot
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
+    case EV_TXSTART:
+      Serial.println(F("EV_TXSTART"));
+      break;
+    case EV_TXCANCELED:
+      Serial.println(F("EV_TXCANCELED"));
+      break;
+    case EV_RXSTART:
+      /* do not print anything -- it wrecks timing */
+      break;
+    case EV_JOIN_TXCOMPLETE:
+      Serial.println(F("EV_JOIN_TXCOMPLETE: no JoinAccept"));
+      break;
+
+    default:
+      Serial.print(F("Unknown event: "));
+      Serial.println((unsigned)ev);
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("Starting"));
+
+  // LMIC init
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+
+  LMIC_selectSubBand(1);
+
+  // Start job (sending automatically starts OTAA too)
+  do_send(&sendjob);
+}
+
+void loop() { os_runloop_once(); }
